aa7fdc368bab1e4aaf097e3b984f6f4ba28756c7
rwiv <zc798546@gmail.com>
2025-02-17 16:09:43 +0900

    chore: remove unused scripts

--- scripts/curl_whsync.js
+++
@@ -1,8 +0,0 @@
-async function main() {
-  const [endpoint] = process.argv.slice(2);
-  let method = "POST";
-  const res = await fetch(`${endpoint}/api/webhooks/sync`, {method});
-  console.log(await res.text());
-}
-
-main().catch(console.error);
